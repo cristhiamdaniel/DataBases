40016bcad64dc489a14c51a70c2ca55665093e8f
cristhiamdaniel <danielcampos.ingenieria@gmail.com>
2022-04-08 16:19:40 -0500

    funciones para errores

--- error.py
+++ error.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 
+'''
 try:
     mi_conexion = mysql.connector.connect(
         host = input("introduce la IP: "),
@@ -19,3 +20,61 @@ try:
 except Error as e:
     print("Ha ocurrido un error:")
     print(e)
+'''
+
+#Usando funciones
+
+def conectarse(ip, usuario, contrasenia):
+    try:
+        conexion = mysql.connector.connect(
+            host = ip,
+            user = usuario,
+            passwd = contrasenia)
+        print("Conexion realizada correctamente")
+
+    except Error as e:
+        conexion = None
+        print("Ha ocurrido el error:")
+        print(e)
+
+    return conexion
+
+def desconectarse(conexion):
+    if conexion:
+        conexion.close()
+        print("Conexion finalizada correctamente.")
+
+def hacer_consulta(conexion):
+    cursor = conexion.cursor()
+    cursor.execute("SHOW DATABASES")
+    for bd in cursor:
+        print(bd)
+    cursor.close()
+
+i = input("Introduzca la ip: ")
+u = input("Introduzca el usuario: ")
+c = input("Introduzca la contrasenia: ")
+
+conexion = conectarse(i,u,c)
+if conexion:
+    hacer_consulta(conexion)
+    desconectarse(conexion)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
